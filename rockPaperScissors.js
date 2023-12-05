034e760370c79b37f07be5e84dd9d94802378b79
Tyrone <tschoeman7@gmail.com>
2023-12-05 20:07:30 +0200

    added function to allow for randomization of numbers, each corresponding to rock, paper or scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+// Get random computer choice
+
+let getComputerChoice = function () {
+  let randNum = Math.floor(Math.random() * 3) + 1;
+  let rock = 1;
+  let paper = 2;
+  let scissors = 3;
+  if (randNum === rock) {
+    return "rock";
+  } else if (randNum === paper) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+// //Initialize variables
+// let playerSelection = "rock";
+// let computerChoice = getComputerChoice();
+
+// // Function to repeat the play
+// function playRound(playerSelection, computerChoice) {
+//   if (playerSelection === "rock" && computerChoice === "scissors") {
+//     console.log("Computer chose scissors you chose rock you win!");
+//   }
+// }
+
+// playRound(playerSelection, computerChoice);
