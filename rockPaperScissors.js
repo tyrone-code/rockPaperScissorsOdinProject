82e8e9f5b769b87b5004cd8e920c34a2f8912281
Tyrone <tschoeman7@gmail.com>
2023-12-11 17:32:42 +0200

    finished rough draft of rock, paper scissors, from odin project.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -19,7 +19,7 @@ let getComputerChoice = function () {
 let computerScore = 0;
 let userScore = 0;
 function playRound() {
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 5; i++) {
     let selection = prompt("type either rock, paper or scissors");
     let userSelection = selection.toLowerCase();
     let computerSelection = getComputerChoice();
@@ -44,64 +44,46 @@ function playRound() {
       computerScore += 1;
       console.log(`User: ${userScore}`);
       console.log(`Computer: ${computerScore}`);
+    } else if (userSelection === "paper" && computerSelection === "rock") {
+      console.log(
+        `you chose ${userSelection} and the computer chose ${computerSelection} you win!`
+      );
+      userScore += 1;
+      console.log(`User: ${userScore}`);
+      console.log(`Computer: ${computerScore}`);
+    } else if (userSelection === "paper" && computerSelection === "scissors") {
+      console.log(
+        `you chose ${userSelection} and the computer chose ${computerSelection} you lose!`
+      );
+      computerScore += 1;
+      console.log(`User: ${userScore}`);
+      console.log(`Computer: ${computerScore}`);
+    } else if (userSelection === "scissors" && computerSelection === "paper") {
+      console.log(
+        `you chose ${userSelection} and the computer chose ${computerSelection} you win!`
+      );
+      userScore += 1;
+      console.log(`User: ${userScore}`);
+      console.log(`Computer: ${computerScore}`);
+    } else if (userSelection === "scissors" && computerSelection === "rock") {
+      console.log(
+        `you chose ${userSelection} and the computer chose ${computerSelection} you lose!`
+      );
+      computerScore += 1;
+      console.log(`User: ${userScore}`);
+      console.log(`Computer: ${computerScore}`);
     }
   }
 }
-// } else if (userSelection === "rock" && computerSelection === "scissors") {
-//   userScore += 1;
-//   console.log(`user: ${userScore}`);
-//   return "Computer chose scissors you chose rock you win!";
-// } else if (userSelection === "rock" && computerSelection === "paper") {
-//   return "Computer chose paper you chose rock you lose!";
-// } else if (userSelection === "paper" && computerSelection === "rock") {
-//   userScore += 1;
-//   console.log(`user: ${userScore}`);
-//   return "You chose paper computer chose rock, you win!";
-// } else if (userSelection === "paper" && computerSelection === "scissors") {
-//   return "You chose paper computer chose scissors, you lose!";
-// } else if (userSelection === "scissors" && computerSelection === "rock") {
-//   return "You chose scissors computer chose rock, you lose!";
-// } else if (userSelection === "scissors" && computerSelection === "paper") {
-//   userScore += 1;
-//   console.log(`user: ${userScore}`);
-//   return "You chose scissors computer chose paper, you win!";
-// }
-console.log(playRound());
 
-// function roundScore() {
+playRound();
 
-// let game = function () {
-//   console.log(playRound());
-// console.log(`Computer: ${computerScore}`);
-//   console.log(`user: ${userScore}`);
-//   console.log(playRound());
-//   console.log(`Computer: ${computerScore}`);
-// console.log(`user: ${userScore}`);
-//   console.log(playRound());
-//   console.log(`Computer: ${computerScore}`);
-//   console.log(`user: ${userScore}`);
-//   console.log(playRound());
-//   console.log(`Computer: ${computerScore}`);
-//   console.log(`user: ${userScore}`);
-//   console.log(playRound());
-
-//   console.log(`Computer: ${computerScore}`);
-//   console.log(`user: ${userScore}`);
-// };
-
-// console.log(game());
-
-// function roundScore() {
-//   let playRound = "You win!";
-//   if (playRound === "Tie!") {
-//     computerScore += 0;
-//   } else if (playRound === "You win!") {
-//     userScore += 1;
-//   } else if (playRoundResult === "You lose!") {
-//     computerScore += 1;
-//   }
-//   console.log(`Computer: ${computerScore}`);
-//   console.log(`user: ${userScore}`);
-// }
-
-// console.log(roundScore());
+if (userScore > computerScore) {
+  alert(`Out of 5 games you win! Your score was ${userScore} and the computer score was ${computerScore}
+  congratulations! `);
+} else if (userScore < computerScore) {
+  alert(`Out of 5 games you lose! Your score was ${userScore} and the computer score was ${computerScore}
+  try Again!`);
+} else {
+  alert("it was a draw! Try again!");
+}
